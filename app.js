e20d5a9db21fc01ce4e66fd9bd29e7d4fad36b2d
Romina Vargas <rsocci@me.com>
2013-11-05 13:13:37 -0500

    Start Ember application and add 'roll' resource

---
+++ app.js
@@ -0,0 +1,12 @@
+var Roller = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_BINDINGS: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_STACKTRACE_ON_DEPRECATION: true,
+  LOG_VERSION: true,
+  debugMode: true
+});
+
+Roller.Router.map(function() {
+  this.resource('roll');
+});
