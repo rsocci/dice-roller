44c772163487ea075c587b060617a71ac1838ec4
Romina Vargas <rsocci@me.com>
2013-11-05 14:03:54 -0500

    Add roll model

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ var Roller = Ember.Application.create({
   debugMode: true
 });
 
+Roller.Roll = Ember.Object.extend({
+  diceNumber: 0,
+  totalRolls: 0,
+  numberOfRolls: 0,
+
+  proportion: function() {
+    var width = 50 + parseInt(400 * this.get("numberOfRolls") /
+      this.get("totalRolls"));
+    return "width: " + width + "px;";
+  }.property("totalRolls", "numberOfRolls")
+});
+
 Roller.Router.map(function() {
   this.resource('roll');
 });
